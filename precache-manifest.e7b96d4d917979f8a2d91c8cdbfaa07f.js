adc9a63bf3f2a48d430c80ac83625881e191fca5
axothic <axothic@gmail.com>
2020-09-21 17:18:44 +0200

    Initial Commit

---
+++ precache-manifest.e7b96d4d917979f8a2d91c8cdbfaa07f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854cfb198df66061c3e8bd71aeb4a52f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c50772e2ea2a265b20c2",
+    "url": "/static/css/main.1b322b0f.chunk.css"
+  },
+  {
+    "revision": "66932e5e488f8ec438a0",
+    "url": "/static/js/2.79eb2586.chunk.js"
+  },
+  {
+    "revision": "7880e528e8c21721ffc6e103c03ca023",
+    "url": "/static/js/2.79eb2586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c50772e2ea2a265b20c2",
+    "url": "/static/js/main.b806338c.chunk.js"
+  },
+  {
+    "revision": "bb5034086d3769d0457d",
+    "url": "/static/js/runtime-main.73eabe50.js"
+  }
+]);
